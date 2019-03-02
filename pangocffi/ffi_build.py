57556491540fdeeab871ea08913c82f75846d43f
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-03-02 21:26:56 +0000

    Moved out glib cdefs into their own txt file.

--- pangocffi/ffi_build.py
+++ pangocffi/ffi_build.py
@@ -14,40 +14,19 @@ sys.path.append(str(Path(__file__).parent))
 
 # Create an empty _generated folder if needed
 (Path(__file__).parent / '_generated').mkdir(exist_ok=True)
-cdefs_file = open(Path(__file__).parent / 'cdefs.txt', 'r')
-cdefs = cdefs_file.read()
+
+# Read the CFFI definitions
+cdefs_glib_file = open(Path(__file__).parent / 'cdefs_glib.txt', 'r')
+cdefs_glib = cdefs_glib_file.read()
+cdefs_pango_file = open(Path(__file__).parent / 'cdefs_pango.txt', 'r')
+cdefs_pango = cdefs_pango_file.read()
 
 # cffi definitions, in the order outlined in:
 # https://developer.gnome.org/pango/stable
 ffi = FFI()
 ffi.set_source('pangocffi._generated.ffi', None)
-ffi.cdef("""
-    typedef ... GType;
-    typedef unsigned int guint8;
-    typedef unsigned int guint16;
-    typedef unsigned int guint32;
-    typedef unsigned int guint;
-    typedef int gint;
-    typedef int gint32;
-    typedef gint gboolean;
-    typedef char gchar;
-    typedef unsigned char guchar;
-    typedef ... gunichar;
-    typedef void* gpointer;
-    typedef ... gconstpointer;
-    typedef ... GObject;
-    typedef ... GObjectClass;
-    typedef ... GString;
-    typedef ... GDestroyNotify;
-    typedef ... GList;
-    typedef ... GSList;
-    typedef ... GError;
-    typedef ... GMarkupParseContext;
-    typedef ... GTypeModule;
-    
-    void g_object_unref (gpointer object);
-""")
-ffi.cdef(cdefs)
+ffi.cdef(cdefs_glib)
+ffi.cdef(cdefs_pango)
 
 if __name__ == '__main__':
     ffi.compile()
