5374df285c6d69100bf11a1ec4c3f78395a8a412
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-03-03 11:48:58 +0000

    removed @dataclass to support python 3.5 compatibility

--- pangocffi/rectangle.py
+++ pangocffi/rectangle.py
@@ -1,18 +1,17 @@
 import ctypes
-from dataclasses import dataclass
 
 
-@dataclass
 class Rectangle:
     """
     The :class:`Rectangle` structure represents a rectangle. It is
     frequently used to represent the logical or ink extents of a single glyph
     or section of text.
     """
-    x: int
-    y: int
-    width: int
-    height: int
+    def __init__(self, x: int, y: int, width: int, height: int):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
 
     @staticmethod
     def from_pointer(pointer: ctypes.c_void_p) -> 'Rectangle':
