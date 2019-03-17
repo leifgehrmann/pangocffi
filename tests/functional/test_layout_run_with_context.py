e1ccd8edea4dcfe29e5506301c58465ecf96920c
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-03-17 18:28:54 +0000

    WIP: Fix all other tests

--- tests/functional/test_layout_run_with_context.py
+++ tests/functional/test_layout_run_with_context.py
@@ -4,26 +4,33 @@ import unittest
 
 
 class TestLayoutRunWithContext(unittest.TestCase):
-    pass
-    # def test_layout_run_from_pointer(self):
-    #     layout = Layout(create_pango_context())
-    #     layout.set_text('Hello World')
-    #     layout_iter = layout.get_iter()
-    #
-    #     layout_run = layout_iter.get_run()
-    #     same_layout = LayoutRun.from_pointer(layout_run.get_pointer())
-    #
-    #     assert isinstance(same_layout, LayoutRun)
-    #
-    # def test_layout_run_from_null_pointer(self):
-    #     with self.assertRaises(ValueError):
-    #         LayoutRun.from_pointer(ffi.NULL)
-    #
-    # def test_layout_run_properties(self):
-    #     layout = Layout(create_pango_context())
-    #     layout.set_text('Hello World')
-    #     layout_iter = layout.get_iter()
-    #
-    #     layout_run = layout_iter.get_run()
-    #     layout_run_item = layout_run.item
-    #     assert isinstance(layout_run_item, Item)
+
+    def setUp(self):
+        self.pango_context = ContextCreator.get_pango_context()
+
+    def tearDown(self):
+        self.pango_context = None
+        ContextCreator.free()
+
+    def test_layout_run_from_pointer(self):
+        layout = Layout(self.pango_context)
+        layout.set_text('Hello World')
+        layout_iter = layout.get_iter()
+
+        layout_run = layout_iter.get_run()
+        same_layout = LayoutRun.from_pointer(layout_run.get_pointer())
+
+        assert isinstance(same_layout, LayoutRun)
+
+    def test_layout_run_from_null_pointer(self):
+        with self.assertRaises(ValueError):
+            LayoutRun.from_pointer(ffi.NULL)
+
+    def test_layout_run_properties(self):
+        layout = Layout(self.pango_context)
+        layout.set_text('Hello World')
+        layout_iter = layout.get_iter()
+
+        layout_run = layout_iter.get_run()
+        layout_run_item = layout_run.item
+        assert isinstance(layout_run_item, Item)
--- tests/functional/test_layout_with_context.py
+++ tests/functional/test_layout_with_context.py
@@ -4,13 +4,18 @@ import unittest
 
 
 class TestLayoutWithContext(unittest.TestCase):
-    pass
-    # @staticmethod
-    # def test_layout_setting_text():
-    #     layout = Layout(create_pango_context())
-    #
-    #     layout.set_text('Hi from Παν語')
-    #     assert layout.get_text() == 'Hi from Παν語'
-    #
-    #     layout.set_markup('<span font="italic 30">Hello from Παν語</span>')
-    #     assert layout.get_text() == 'Hello from Παν語'
+    def setUp(self):
+        self.pango_context = ContextCreator.get_pango_context()
+
+    def tearDown(self):
+        self.pango_context = None
+        ContextCreator.free()
+
+    def test_layout_setting_text(self):
+        layout = Layout(self.pango_context)
+
+        layout.set_text('Hi from Παν語')
+        assert layout.get_text() == 'Hi from Παν語'
+
+        layout.set_markup('<span font="italic 30">Hello from Παν語</span>')
+        assert layout.get_text() == 'Hello from Παν語'
