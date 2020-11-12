4179533fd2bbaee25088469a9425d933ddf3cd9c
Leif Gehrmann <leif.gehrmann@gmail.com>
2020-11-12 17:58:22 +0000

    Removing unnecessary setuptools dep

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import sys
-import setuptools
 
 if sys.version_info.major < 3:
     raise RuntimeError(
         'pangocffi does not support Python 2.x. Please use Python 3.'
     )
-
-setuptools.setup()
