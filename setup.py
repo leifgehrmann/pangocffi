333cc052d76ba690bfc22c6831ab7bca9d03c8b8
Leif Gehrmann <leif.gehrmann@gmail.com>
2020-11-12 18:31:19 +0000

    Revert "Removing unnecessary setuptools dep"
    This reverts commit 2dcdda7b8158d84d68d3d3defb9a759042413426.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import sys
+import setuptools
 
 if sys.version_info.major < 3:
     raise RuntimeError(
         'pangocffi does not support Python 2.x. Please use Python 3.'
     )
+
+setuptools.setup()
