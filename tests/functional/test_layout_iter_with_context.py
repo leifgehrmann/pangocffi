d0e8833cb1e950c262059d3a3517f0441af950df
Leif Gehrmann <leif.gehrmann@gmail.com>
2020-12-20 22:11:39 +0000

    Improved test coverage

--- tests/functional/test_layout_iter_with_context.py
+++ tests/functional/test_layout_iter_with_context.py
@@ -18,6 +18,13 @@ class TestLayoutIterWithContext(unittest.TestCase):
         assert isinstance(layout_iter, LayoutIter)
         assert isinstance(layout_iter.get_pointer(), ffi.CData)
 
+        pointer_shallow_copy = layout_iter.get_pointer()
+        layout_iter_shallow_copy = LayoutIter.from_pointer(
+            pointer_shallow_copy
+        )
+        assert isinstance(layout_iter_shallow_copy, LayoutIter)
+        assert isinstance(layout_iter_shallow_copy.get_pointer(), ffi.CData)
+
     def test_layout_iter_properties(self):
         layout = Layout(self.pango_context)
         layout_iter = layout.get_iter()
