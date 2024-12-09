9fb5c61c4b57cdcc0be14aa7b0c1945aa4098825
George Harker <george@george-graphics.co.uk>
2024-12-09 12:06:30 -0800

    simplify api vs abi mode and auto pick

--- pangocffi/ffi_build.py
+++ pangocffi/ffi_build.py
@@ -6,38 +6,98 @@
 
 """
 
-import importlib.util
-import os
+import platform
 import sys
+from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
 from pathlib import Path
+from warnings import warn
 
+from cffi import FFI
+from cffi.error import VerificationError
 
 sys.path.append(str(Path(__file__).parent))
 
-api_mode = False
-if ('PANGOCFFI_API_MODE' in os.environ and
-        int(os.environ['PANGOCFFI_API_MODE']) == 1):
-    # Allow explicit disable of api_mode
-    api_mode = True
 
-# Create an empty _generated folder if needed
-if not api_mode:
-    (Path(__file__).parent / '_generated').mkdir(exist_ok=True)
+def ffi_for_mode(mode):
+    # Read the C definitions
+    c_definitions_glib_file = open(
+        str(Path(__file__).parent / 'c_definitions_glib.txt'),
+        'r'
+    )
+    c_definitions_pango_file = open(
+        str(Path(__file__).parent / 'c_definitions_pango.txt'),
+        'r'
+    )
+    c_definitions_glib = c_definitions_glib_file.read()
+    c_definitions_pango = c_definitions_pango_file.read()
 
-# Because we can't directly load the instance builder (it would run
-# ``__init__.py`` for any module import) we have to do this dubious import.
-spec = importlib.util.spec_from_file_location(
-    'ffi_instance_builder',
-    str(Path(__file__).parent / 'ffi_instance_builder.py')
-)
-ffi_instance_builder = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(ffi_instance_builder)
+    ffi = FFI()
+    # Mirror the GType setup in gobject/gtypes.h
+    if ffi.sizeof('void*') > ffi.sizeof('long'):
+        ffi.cdef('typedef unsigned int* GType;')
+    else:
+        ffi.cdef('typedef unsigned long GType;')
+    ffi.cdef(c_definitions_glib)
+    ffi.cdef(c_definitions_pango)
+    if mode == "api":
+        ffi.set_source_pkgconfig(
+            "_pangocffi",
+            ['pango', 'glib-2.0', 'pangoft2'] +
+            (['pangoxft'] if platform.system() == 'Linux' else []),
+            r"""
+            #include "glib.h"
+            #include "glib-object.h"
+            #include "pango/pango.h"
+            #include "pango/pango-fontmap.h"
 
-# Generate the bindings
-ffiBuilder = ffi_instance_builder.FFIInstanceBuilder(
-    source='pangocffi._generated.ffi' if not api_mode else None
-)
-ffi = ffiBuilder.generate()
+            #include <stdio.h>
+            #if PANGO_VERSION < G_ENCODE_VERSION(1, 54)
+            int pango_item_get_char_offset (
+              PangoItem* item) {
+                fprintf(stderr, "Unimplemented!!\n");
+                return -1;
+            }
+            #endif
+            #if PANGO_VERSION < G_ENCODE_VERSION(1, 52)
+            PangoFont*
+            pango_font_map_reload_font (
+              PangoFontMap* fontmap,
+              PangoFont* font,
+              double scale,
+              PangoContext* context,
+              const char* variations) {
+                fprintf(stderr, "Unimplemented!!\n");
+                return NULL;
+            }
+
+            #endif
+            """,
+            sources=[]
+        )
+    else:
+        ffi.set_source("_pangocffi", None)
+    return ffi
+
+
+def build_ffi():
+    """
+    This will be called from setup() to return an FFI
+    which it will compile - work out here which type is
+    possible and return it.
+    """
+    try:
+        ffi_api = ffi_for_mode("api")
+        ffi_api.compile(verbose=True)
+        return ffi_api
+    except (CCompilerError, DistutilsExecError, DistutilsPlatformError,
+            VerificationError) as e:
+        warn("Falling back to precompiled python mode: {}".format(str(e)))
+
+        ffi_abi = ffi_for_mode("abi")
+        ffi_abi.compile(verbose=True)
+        return ffi_abi
+
 
 if __name__ == '__main__':
+    ffi = build_ffi()
     ffi.compile()
--- setup.py
+++ setup.py
@@ -9,17 +9,12 @@ if sys.version_info.major < 3:
         'pangocffi does not support Python 2.x. Please use Python 3.'
     )
 
-api_mode = False
-if ('PANGOCFFI_API_MODE' in os.environ and
-        int(os.environ['PANGOCFFI_API_MODE']) == 1):
-    api_mode = True
-
 setup(
     name='pangocffi',
     use_scm_version=True,
     install_requires=['cffi >= 1.1.0'],
     setup_requires=['cffi >= 1.1.0'],
     packages=['pangocffi'],
-    cffi_modules=['pangocffi/ffi_build.py:ffi'] if api_mode else []
+    cffi_modules=['pangocffi/ffi_build.py:build_ffi']
 )
 
